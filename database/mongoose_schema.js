49bb62a26a3c3d6d53efa4621784df4a8defbe8c
The Anh <nhoxtheanh@gmail.com>
2020-10-28 01:10:40 +0700

    add Column schema & update DB

--- database/mongoose_schema.js
+++ database/mongoose_schema.js
@@ -21,7 +21,7 @@ mongoose.model("User", userSchema);
 
 // Board
 const boardSchema = new mongoose.Schema({
-    ownerID:  { type: SchemaTypes.Long, min: 0, default: 0 },  ////////////////////////////////////////////////////////////////////////////// String or Int???
+    ownerID:  { type: SchemaTypes.Long, min: 0, default: 0 },
     name: { type: String, required: true, maxlength: 128 },
     createdAt: { type: Number, default: Date.now },
     isActive: { type: Boolean, default: true },
@@ -31,10 +31,21 @@ boardSchema.index({ coords: "2dsphere" });
 boardSchema.plugin(AutoIncrement, { inc_field: "boardID" });
 mongoose.model("Board", boardSchema);
 
+// Column
+const columnSchema = new mongoose.Schema({
+    boardID:  { type: SchemaTypes.Long, min: 0, default: 0 },
+    name: { type: String, required: true, maxlength: 128 },
+    color: { type: String, default: 'white' },
+    isActive: { type: Boolean, default: true },
+}, { collection: "Columns" }, { toJSON: { virtuals: true }, toObject: { virtuals: true } });
+
+columnSchema.index({ coords: "2dsphere" });
+columnSchema.plugin(AutoIncrement, { inc_field: "columnID" });
+mongoose.model("Column", columnSchema);
+
 // Card
 const cardSchema = new mongoose.Schema({
-    boardID:  { type: SchemaTypes.Long, min: 0, default: 0 },  ////////////////////////////////////////////////////////////////////////////// String or Int???
-    columnIndex: Number,
+    columnID:  { type: SchemaTypes.Long, min: 0, default: 0 },
     content: String,
     isActive: { type: Boolean, default: true },
 }, { collection: "Cards" }, { toJSON: { virtuals: true }, toObject: { virtuals: true } });
@@ -65,19 +76,59 @@ mongoose.model("Card", cardSchema);
 // var Board = mongoose.model("Board", boardSchema);
 // var BoardData = {
 //     ownerID:  1, 
-//     name: 'Board 2',
-//     //createdAt:   '',
+//     name: 'Board 1',
 // }
 // var boardCollections = new Board(BoardData);
 // boardCollections.save(function (err, data) {
 //     if (err) return console.error(err);
 // });
 
+// var Column = mongoose.model("Column", columnSchema);
+// var ColumnData = {
+//     boardID:  1, 
+//     name: 'Column 1',
+// }
+// var columnCollections = new Column(ColumnData);
+// columnCollections.save(function (err, data) {
+//     if (err) return console.error(err);
+// });
+// var ColumnData2 = {
+//     boardID:  1, 
+//     name: 'Column 2',
+// }
+// var columnCollections2 = new Column(ColumnData2);
+// columnCollections2.save(function (err, data) {
+//     if (err) return console.error(err);
+// });
+// var ColumnData3 = {
+//     boardID:  1, 
+//     name: 'Column 3',
+// }
+// var columnCollections3 = new Column(ColumnData3);
+// columnCollections3.save(function (err, data) {
+//     if (err) return console.error(err);
+// });
+
 // var Card = mongoose.model("Card", cardSchema);
 // var CardData = {
-//     boardID:  1,
-//     columnIndex: 1,
+//     columnID:  1,
 //     content: 'This is the first content.',
+// }
+// var cardCollections = new Card(CardData);
+// cardCollections.save(function (err, data) {
+//     if (err) return console.error(err);
+// });
+// var CardData = {
+//     columnID:  2,
+//     content: 'This is the second content.',
+// }
+// var cardCollections = new Card(CardData);
+// cardCollections.save(function (err, data) {
+//     if (err) return console.error(err);
+// });
+// var CardData = {
+//     columnID:  3,
+//     content: 'This is the third content.',
 // }
 // var cardCollections = new Card(CardData);
 // cardCollections.save(function (err, data) {
