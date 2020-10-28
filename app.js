5a3dc3f1775a195ac89fba1a939b0d0e6f41d305
The Anh <nhoxtheanh@gmail.com>
2020-10-28 21:30:26 +0700

    update CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
+var cors = require('cors');
 
 // Database connection
 require('./database/db');
@@ -9,9 +10,10 @@ require('./database/db');
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // });
+app.use(cors());
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(8080, function () {
+  console.log('App listening on port 8080!');
 });
